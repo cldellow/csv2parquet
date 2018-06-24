7a308bcebc1e70d5fe3bd270702169a3032b2e75
Colin Dellow <cldellow@gmail.com>
2018-06-23 23:41:23 -0400

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # csv2parquet
-Convert a CSV to parquet files
+
+Convert a CSV to a parquet file.
+
+## Usage
+
+First, install the pipenv environment:
+```
+pipenv install
+```
+
+Next, create some Parquet files. The tool supports CSV and TSV files.
+
+```
+./csv2parquet file.csv [--row-group-size NNN] [--output output.parquet]
+```
+
+## csv2tsv
+
+Sorting your files can improve compression and query time. Since CSVs are a pain
+to manipulate, I've included a tool to convert them to TSVs, which can be more
+easily manipulated by standard tools.
+
+```
+./csv2tsv file.csv > file.tsv
+```
+
+`csv2tsv` can also sort, for example, to sort a CSV on its 4th column (but leave
+the header row at the top):
+
+```
+./csv2tsv file.csv -k4 > file.tsv
+```
+
+Under the covers, this delegates to the Unix `sort` command. See `man sort` for other options you can pass.
---
+++ csv2parquet.py
@@ -0,0 +1,70 @@
+import pyarrow.parquet as pq
+import pyarrow as pa
+import argparse
+import csv
+import os
+
+def get_delimiter(csv_file):
+  if csv_file[-4:] == '.tsv':
+    return '\t'
+  return ','
+
+def get_column_names(csv_file):
+  with open(csv_file) as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=get_delimiter(csv_file))
+    rv = []
+    for row in spamreader:
+      for col in row:
+        rv.append(col)
+      return rv
+ 
+def convert(csv_file, output_file, row_group_size):
+  column_names = get_column_names(csv_file)
+  columns = [[] for x in column_names]
+  arrs = [[] for x in column_names]
+
+  def add_arrays(cols):
+    for colnum, col in enumerate(cols):
+      arr = pa.array(col, type=pa.string())
+      arrs[colnum].append(arr)
+
+  with open(csv_file) as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=get_delimiter(csv_file))
+    rownum = -1
+    for row in spamreader:
+      rownum = rownum + 1
+      if rownum == 0:
+        continue
+      idx = -1
+      for v in row:
+        idx += 1
+        columns[idx].append(v)
+      if rownum % 10000 == 0:
+        add_arrays(columns)
+        columns = [[] for x in range(len(column_names))]
+
+  if columns and columns[0]:
+    add_arrays(columns)
+
+  fields = [pa.chunked_array(arr, type=pa.string()) for colnum, arr in enumerate(arrs)]
+  columns = [pa.Column.from_array(column_names[x], fields[x]) for x in range(len(fields))]
+  table = pa.Table.from_arrays(columns)
+
+  pq.write_table(table, output, version='1.0', compression='snappy', use_dictionary=True, row_group_size=row_group_size)
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('csv_file', help="input file, can be CSV or TSV")
+  parser.add_argument('-r', '--row-group-size', default=10000, type=int, help='The number of rows per row group.', nargs='?')
+  parser.add_argument('-o', '--output', help='The parquet file', nargs='?')
+  rv = parser.parse_args()
+  output = rv.output
+  if output is None:
+    output = rv.csv_file
+    if output.endswith('.csv'):
+          output = output[:-4]
+    elif output.endswith('.tsv'):
+          output = output[:-4]
+    output = output + '.parquet'
+  convert(rv.csv_file, output, rv.row_group_size)
+
